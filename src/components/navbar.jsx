f477187430220fa72b34bc2ccd44671467debcf6
Abdallah Hamdy <abdo.aa797@gmail.com>
2022-04-26 22:37:32 +0200

    fix some typo

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -25,7 +25,7 @@ const Navbar = () => {
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
         <Link className="navbar-brand" to={"/"}>
-          Hr sye
+          Hr System
         </Link>
         <button
           className="navbar-toggler"
